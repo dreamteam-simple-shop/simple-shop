05c2d0dee27c27e07358de24e4fbd7ab31c055db
rosalina hundahl <rofr0001@stud.kea.dk>
2025-11-13 11:41:42 +0100

    Import react icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 import "./globals.css";
-import Header from "./components/Header.jsx";
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header />
+      
         {children}
       </body>
     </html>
