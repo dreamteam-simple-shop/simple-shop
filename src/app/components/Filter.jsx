379c1eafe8bb68e0d835aa5133e3ecc8ec2abf0f
rosalina hundahl <rofr0001@stud.kea.dk>
2025-11-19 14:12:05 +0100

    add styling adjustments

--- src/app/components/Filter.jsx
+++ src/app/components/Filter.jsx
@@ -1,62 +1,55 @@
-'use client';
+"use client";
 
-import { Suspense } from 'react';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSearchParams } from 'next/navigation';
+import { Suspense } from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 function FilterButton({ name, category }) {
-	let isActive = false;
-	const router = useRouter();
-	const searchParams = useSearchParams();
-	let currentCategory = searchParams.get('category');
+  let isActive = false;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  let currentCategory = searchParams.get("category");
 
-	if (currentCategory === null) {
-		currentCategory = 'All';
-		isActive = currentCategory === category;
-	} else {
-		isActive = currentCategory === category;
-	}
+  if (currentCategory === null) {
+    currentCategory = "All";
+    isActive = currentCategory === category;
+  } else {
+    isActive = currentCategory === category;
+  }
 
-	return (
-		<button
-			onClick={() => {
-				if (category === 'All') {
-					router.push(`/`);
-				} else {
-					router.push(`/?category=${category}`);
-				}
-			}}
-			className={`
-                rounded-full px-6 py-2 transition-colors duration-300 cursor-pointer
-                ${
-					isActive
-						? 'bg-black text-white'
-						: 'bg-white text-black border border-gray-300 hover:bg-black hover:text-white'
-				}
+  return (
+    <button
+      onClick={() => {
+        if (category === "All") {
+          router.push(`/`);
+        } else {
+          router.push(`/?category=${category}`);
+        }
+      }}
+      className={`
+                rounded-xl px-6 py-2 transition-colors duration-300 cursor-pointer
+                ${isActive ? "bg-black text-white" : "bg-white text-black border border-gray-300 hover:bg-black hover:text-white"}
             `}
-		>
-			{name}
-		</button>
-	);
+    >
+      {name}
+    </button>
+  );
 }
 
 export default function Filter({ title }) {
-	return (
-		<div className="mt-15 md:mt-20 lg:mt-0 flex flex-col gap-8 items-center justify-center">
-			<h1 className="text-xl font-medium">{title}</h1>
-			<p className="text-gray-500 text-center max-w-2xl">
-				Discover our handpicked selection of premium products designed to elevate your
-				everyday style.
-			</p>
-			<div className="flex flex-row gap-4">
-				<Suspense fallback={<div>Loading...</div>}>
-					<FilterButton name="All" category="All" />
-					<FilterButton name="Dresses" category="womens-dresses" />
-					<FilterButton name="Shoes" category="womens-shoes" />
-					<FilterButton name="Bags" category="womens-bags" />
-				</Suspense>
-			</div>
-		</div>
-	);
+  return (
+    <div className="mt-15 md:mt-20 lg:mt-0 flex flex-col gap-8 items-center justify-center">
+      <h1 className="text-xl font-medium">{title}</h1>
+      <p className="text-gray-500 text-center max-w-2xl">Discover our handpicked selection of premium products designed to elevate your everyday style.</p>
+      <div className="flex flex-row gap-4">
+        <Suspense fallback={<div>Loading...</div>}>
+          <FilterButton name="All" category="All" />
+          <FilterButton name="Dresses" category="womens-dresses" />
+          <FilterButton name="Shoes" category="womens-shoes" />
+          <FilterButton name="Bags" category="womens-bags" />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
