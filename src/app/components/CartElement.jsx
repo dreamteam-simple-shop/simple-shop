28e670349dd30b1d5c762c07a47272991f3be3b7
rosalina hundahl <rofr0001@stud.kea.dk>
2025-11-19 11:39:17 +0100

    center cart-button content

--- src/app/components/CartElement.jsx
+++ src/app/components/CartElement.jsx
@@ -8,7 +8,7 @@ const Cart = ({ id }) => {
   if (cart.includes(id)) {
     return (
       <button
-        className="bg-black text-white border border-black py-3 px-6 rounded-4xl self-center flex justify-start items-center gap-3 w-[90%] m-5 hover:bg-white hover:text-black transition-colors duration-300 cursor-pointer"
+        className="bg-black text-white border border-black py-3 px-6 rounded-4xl self-center flex justify-center items-center gap-3 w-[90%] m-5 hover:bg-white hover:text-black transition-colors duration-300 cursor-pointer"
         onClick={(e) => {
           e.preventDefault();
           toggleCart(id);
