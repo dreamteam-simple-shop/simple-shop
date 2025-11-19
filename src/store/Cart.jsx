eeac201101579691a29a332e04e307de2cff4d33
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-11-19 13:55:23 +0100

    Add quantity

--- src/store/Cart.jsx
+++ src/store/Cart.jsx
@@ -1,20 +1,32 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 const useStore = create(
-  persist(
-    (set, get) => ({
-      cart: [],
-      toggleCart: (product) => {
+	persist(
+		(set, get) => ({
+			cart: [],
+      updateCart: (id, price, quantity) => {
         const current = get().cart;
-        const exists = current.includes(product);
+        const exists = current.some((elm) => elm.id === id);
         set({
-          cart: exists ? current.filter((elm) => elm !== product) : [...current, product],
+          cart: exists
+            ? current.map((elm) => elm.id === id ? { ...elm, quantity: quantity } : elm)
+            : [...current, { id, price, quantity }],
         });
       },
-    }),
-    { name: "cart-storage" }
-  )
+			toggleCart: (id, price, quantity) => {
+				const current = get().cart;
+				const exists = current.some((elm) => elm.id === id);
+				set({
+					cart: exists
+						? current.filter((elm) => elm.id !== id)
+						: [...current, { id, price, quantity }],
+				});
+				console.log(get().cart);
+			},
+		}),
+		{ name: 'cart-storage' },
+	),
 );
 
 export default useStore;
